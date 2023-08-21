bca12d890c9f1818547636931392658449c3a154
keshav <keshavsharma1196@gmail.com>
2023-08-21 08:38:30 +0530

    changing config file

--- app/config.py
+++ app/config.py
@@ -3,17 +3,18 @@ import os
 
 load_dotenv()
 
-db_user = os.environ['DB_USER']
-db_password = os.environ['DB_PASSWORD']
-db_host = os.environ['DB_HOST']
-db_port = os.environ['DB_PORT']
-db_name = os.environ['DB_NAME']
+# db_user = os.environ['DB_USER']
+# db_password = os.environ['DB_PASSWORD']
+# db_host = os.environ['DB_HOST']
+# db_port = os.environ['DB_PORT']
+# db_name = os.environ['DB_NAME']
 # SQLALCHEMY_DATABASE_URI = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
 
 class Config:
     SECRET_KEY = os.environ["SECRET_KEY"]
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+    # SQLALCHEMY_DATABASE_URI = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+    SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
     FLASK_DEBUG = True
     DB_USER = 'postgres'
     DB_PASSWORD = os.environ["DB_PASSWORD"]
